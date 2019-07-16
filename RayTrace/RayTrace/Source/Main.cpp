af1c949f1cb2f3cf60ad43738b9010364cad7350
suudai <34059792+n-suudai@users.noreply.github.com>
2019-07-16 18:08:54 +0900

    【RayTrace】 プロジェクトを追加

---
+++ RayTrace/RayTrace/Source/Main.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <fstream>
+
+
+#define UNUSED(x) (x)
+
+
+int main(int argc, char* argv[])
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    const int width = 1280;
+    const int height = 720;
+
+    std::ofstream ofs("result.ppm");
+
+    ofs << "P3\n" << width << " " << height << "\n255\n";
+
+    for (int i = 0; i < width * height; i++)
+    {
+        ofs << "255 0 255\n";
+    }
+
+    return 0;
+}
+
