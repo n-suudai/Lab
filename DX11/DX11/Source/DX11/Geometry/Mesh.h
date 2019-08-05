ea04879264d0ecdadd4ad6daabdd1a1d7e3a75d4
suudai <34059792+n-suudai@users.noreply.github.com>
2019-08-06 06:55:26 +0900

    【DX11】 コミット漏れ

---
+++ DX11/DX11/Source/DX11/Geometry/Mesh.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <string>
+#include "DX11/External/glm/glm_Prerequisites.h"
+#include "DX11/DX11Prerequisites.h"
+
+
+class Mesh
+{
+public:
+    struct Vertex
+    {
+        glm::vec4 Position;
+        glm::vec4 Color;
+        glm::vec4 Normal;
+        glm::vec2 TexCoord;
+    };
+
+    Mesh(
+        const ComPtr<ID3D11Device>& device,
+        const ComPtr<ID3D11DeviceContext>& context
+    );
+    virtual ~Mesh();
+
+protected:
+    std::string m_FileName;
+
+
+};
+
